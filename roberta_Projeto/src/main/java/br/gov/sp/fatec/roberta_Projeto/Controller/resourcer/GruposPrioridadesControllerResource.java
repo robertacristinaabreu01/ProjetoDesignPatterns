f60994eccc6bab52705d086b7017a949c0255852
Roberta Abreu <76497778+robertacristinaabreu01@users.noreply.github.com>
2021-03-24 22:21:16 -0300

    add

--- roberta_Projeto/src/main/java/br/gov/sp/fatec/roberta_Projeto/Controller/resourcer/GruposPrioridadesControllerResource.java
+++ roberta_Projeto/src/main/java/br/gov/sp/fatec/roberta_Projeto/Controller/resourcer/GruposPrioridadesControllerResource.java
@@ -5,9 +5,6 @@ package br.gov.sp.fatec.roberta_Projeto.Controller.resourcer;
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.PathVariable;
-	import org.springframework.web.bind.annotation.PostMapping;
-	import org.springframework.web.bind.annotation.PutMapping;
-	import org.springframework.web.bind.annotation.RequestBody;
 	import org.springframework.web.bind.annotation.RequestMapping;
 	import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +12,7 @@ package br.gov.sp.fatec.roberta_Projeto.Controller.resourcer;
 	import br.gov.sp.fatec.roberta_Projeto.repository.GruposPrioridadesRepository;
 
 	@RestController
-	@RequestMapping("/pessoas")
+	@RequestMapping("/grupos-prioridades")
 	public class GruposPrioridadesControllerResource {
 
 		@Autowired
@@ -26,24 +23,8 @@ package br.gov.sp.fatec.roberta_Projeto.Controller.resourcer;
 			return GruposPrioridadesRepository.findAll();
 		}
 
-		@PostMapping
-		public GruposPrioridades cadastrarPessoa(@RequestBody GruposPrioridades GruposPrioridades) {
-			return GruposPrioridadesRepository.save(GruposPrioridades);
-		}
-
-		@PutMapping("/{codigo}")
-		public GruposPrioridades atualizar(@PathVariable("codigo") Long codigo, @RequestBody GruposPrioridades GruposPrioridades) {
-			return GruposPrioridadesRepository.findById(codigo).map(record -> {
-				record.setCodigo(GruposPrioridades.getCodigo());
-				record.setDescricao(GruposPrioridades.getDescricao());
-				record.setNome(GruposPrioridades.getNome());
-				return GruposPrioridadesRepository.save(record);
-			}).orElse(null);
-		}
-
 		@GetMapping("/{codigo}")
 		public GruposPrioridades buscarPeloCodigo(@PathVariable Long codigo) {
 			return GruposPrioridadesRepository.findById(codigo).orElse(null);
 		}
 	}
-
