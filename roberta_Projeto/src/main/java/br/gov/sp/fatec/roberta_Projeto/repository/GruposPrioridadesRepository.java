5be9615c4d3ee60e69ef6d18de27c25ebadaa31e
Roberta Abreu <76497778+robertacristinaabreu01@users.noreply.github.com>
2021-04-01 01:24:21 -0300

    delete

--- roberta_Projeto/src/main/java/br/gov/sp/fatec/roberta_Projeto/repository/GruposPrioridadesRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.sp.fatec.roberta_Projeto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.gov.sp.fatec.roberta_Projeto.domain.GruposPrioridades;
-
-public interface GruposPrioridadesRepository extends JpaRepository<GruposPrioridades,Long>{
-
-}
